68431bf71884af9f6da63d7601f96be81af408e3
Raymond Yin <ryin@seas.upenn.edu>
2015-07-04 17:52:54 -0400

    ported back to python3, added metadata

--- dl.py
+++ dl.py
@@ -1,12 +1,15 @@
 from bs4 import BeautifulSoup
 from clint.textui import puts, colored, progress
+import mutagen
+from mutagen.mp3 import MP3
+from mutagen.easyid3 import EasyID3
 
 import requests
 import os
 import sys
 import code
 import re
-
+import time
 
 def format(s):
     return s.lower().replace(' ', '_')
@@ -15,15 +18,50 @@ def format(s):
 def download_file(url, filename):
     print('DOWNLOADING {}'.format(filename))
     r_dl = requests.get(url, stream=True)
+    size, count = int(r_dl.headers['content-length']), 0
+    while size == 165:
+        if count >= 50:
+            print('Error downloading file. Please try again.')
+            sys.exit()
+        r_dl = requests.get(url, stream=True)
+        size = int(r_dl.headers['content-length'])
+        count += 1
     with open(filename, 'wb') as f:
-        total_length = int(r_dl.headers['content-length'])
-        for chunk in progress.bar(r_dl.iter_content(chunk_size=1024), expected_size=(total_length / 1024) + 1):
+        for chunk in progress.bar(r_dl.iter_content(chunk_size=1024), expected_size=(size / 1024) + 1):
             if chunk:
                 f.write(chunk)
                 f.flush()
 
+def print_info(filename):
+    mp3file = MP3(filename)
+    try:
+        meta = EasyID3(filename)
+    except mutagen.id3.ID3NoHeaderError:
+        meta = mutagen.File(filename, easy=True)
+        meta.add_tags()
+
+    min_sec = time.strftime("%M:%S", time.gmtime(mp3file.info.length))
+
+    bitrate = int(mp3file.info.bitrate/1000)
+
+    basic_info = '{}: {} ({} Mbps)'.format(filename, min_sec, bitrate)
+    puts(colored.green(basic_info))
+
+    meta['title'] = meta.get('title') or input('Title: ')
+    meta['artist'] = meta.get('artist') or input('Artist: ')
+    meta['album'] = meta.get('album') or input('Album: ')
+    meta.save()
+
+    title = meta['title'][0] or '[No Title]'
+    artist = meta['artist'][0] or '[No Artist]'
+    album = meta['album'][0] or '[No Album]'
+
+    id3_info = '\t{}\n\t{} - {}'.format(title, artist, album)
+
+    puts(colored.green(id3_info))
+
 if len(sys.argv) < 2:
-    print('Usage: python music_downloader.py [SEARCH TERM]')
+    print('Usage: python dl.py [SEARCH TERM]')
     sys.exit()
 
 query = ' '.join(sys.argv[1:])
@@ -46,14 +84,17 @@ for k, v in options.items():
 
 opt = 'N'
 while opt != 'Y':
-    choice = int(raw_input('Enter index of song: '))
+    choice = int(input('Enter index of song: '))
     chosen_link = options[choice][2]
-    size = 0.000165
+    size, count = 0.000165, 0
     while size == 0.000165:
+        if count >= 30:
+            break
         r = requests.head(chosen_link)
-        size = int(r.headers['content-length']) / 1000000
+        size = int(r.headers['content-length'])/1000000
+        count += 1
     puts(colored.cyan('The file is {} MB. Proceed (Y/N)?'.format(size)))
-    opt = raw_input('> ')
+    opt = input('> ')
 
 filename_search = re.search('track/.*/(.*.mp3)', chosen_link)
 if filename_search:
@@ -61,7 +102,9 @@ if filename_search:
 
 puts(colored.cyan('Name your file ({}):'.format(default_name)))
 
-file_name = raw_input('> ')
+file_name = input('> ')
 file_name = file_name or default_name
 
 download_file(chosen_link, file_name)
+
+print_info(file_name)
